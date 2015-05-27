5bedd3504c3889de5e3a54021f4e20f24f0c9a20
Roman Melnyk <aywengo@gmail.com>
2015-05-27 19:29:35 +0200

    added final implementation Replica and Replicator

--- kvstore/src/main/scala/kvstore/Replica.scala
+++ kvstore/src/main/scala/kvstore/Replica.scala
@@ -1,47 +1,42 @@
 package kvstore
 
-import akka.actor._
-import kvstore.Arbiter._
-import kvstore.Persistence.{Persisted, Persist, PersistenceException}
-import kvstore.Replica._
-import kvstore.Replicator.{Replicated, Replicate, SnapshotAck, Snapshot}
+import scala.concurrent.duration.DurationInt
+import scala.language.postfixOps
+
+import akka.actor.Actor
+import akka.actor.ActorRef
+import akka.actor.Cancellable
+import akka.actor.PoisonPill
+import akka.actor.Props
+import akka.actor.actorRef2Scala
+import akka.event.LoggingReceive
+import kvstore.Arbiter.Join
+import kvstore.Arbiter.JoinedPrimary
+import kvstore.Arbiter.JoinedSecondary
+import kvstore.Arbiter.Replicas
 
 object Replica {
-
   sealed trait Operation {
     def key: String
-
     def id: Long
   }
-
   case class Insert(key: String, value: String, id: Long) extends Operation
-
   case class Remove(key: String, id: Long) extends Operation
-
   case class Get(key: String, id: Long) extends Operation
 
   sealed trait OperationReply
-
   case class OperationAck(id: Long) extends OperationReply
-
   case class OperationFailed(id: Long) extends OperationReply
-
   case class GetResult(key: String, valueOption: Option[String], id: Long) extends OperationReply
 
   def props(arbiter: ActorRef, persistenceProps: Props): Props = Props(new Replica(arbiter, persistenceProps))
 }
 
 class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
-
-  import scala.concurrent.duration._
-  import scala.concurrent.ExecutionContext.Implicits.global
-  import scala.language.postfixOps
-
-  //msg use to retry persist
-  case class ResendPersist(msg:Persist)
-
-  //msg use to manage global acknowledgement
-  case class FailGlobalAck(id:Long)
+  import Replica._
+  import Replicator._
+  import Persistence._
+  import context.dispatcher
 
   /*
    * The contents of this actor is just a suggestion, you can implement it in any way you like.
@@ -50,179 +45,155 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
   var kv = Map.empty[String, String]
   // a map from secondary replicas to replicators
   var secondaries = Map.empty[ActorRef, ActorRef]
-
   // the current set of replicators
-  //var replicators = Set.empty[ActorRef]
+  var replicators = Set.empty[ActorRef]
 
-  //
-  var expectedSeq = 0L
+  var snapshotSeq = 0
+  var persistAcks = Map.empty[Long, ActorRef]
+  var replicateAcks = Map.empty[Long, (ActorRef, Set[ActorRef])]
+  var persistRepeaters = Map.empty[Long, Cancellable]
+  var failureGenerators = Map.empty[Long, Cancellable]
 
-  var persistence = context.actorOf(persistenceProps)
-
-  var persisted = Set.empty[Long]
-
-  //a map of ops id to a Set of Actor acknowledging persistence
-  var globalAcks = Map.empty[Long,Set[ActorRef]]
-
-  var ackRequesters = Map.empty[Long,ActorRef]
-
-  //a set of operations already acknowledged
-  var alreadyAcknoweledged = Set.empty[Long]
-
-  var replicator:ActorRef = _
-
-  //join @ creation
   arbiter ! Join
 
-  override val supervisorStrategy = OneForOneStrategy() {
-    case _: PersistenceException => SupervisorStrategy.resume
-  }
+  val persistence = context.actorOf(persistenceProps)
 
-  def receive = {
+  def receive = LoggingReceive {
     case JoinedPrimary => context.become(leader)
     case JoinedSecondary => context.become(replica)
   }
 
-  val common : Receive = {
-    case Get(key, id) =>
-      sender ! GetResult(key, kv.get(key), id)
+  case class GenerateFailure(id: Long)
 
-    case ResendPersist(persist) =>
-      if(!persisted(persist.id)){
-        persistence ! persist
-        context.system.scheduler.scheduleOnce(100 milliseconds, self, ResendPersist(persist))
-      }
-
-  }
-
+  /* Behavior for  the leader role. */
   val leader: Receive = {
 
-    common.orElse {
+    case Get(key, id) =>
+      val valueOption = kv.get(key)
+      sender ! GetResult(key, valueOption, id)
 
-      case Insert(key, value, id) =>
-        kv = kv + ((key, value))
-        update(Insert(key,value,id),Some(value))
-
-      case Remove(key, id) =>
-        kv = kv - key
-        update(Remove(key,id),None)
-
-      case Persisted(key, id) =>
-        println(s"Persisted key $key id $id")
-        persisted = persisted + id
-        globalAcks = globalAcks.updated(id,globalAcks.get(id).get - self)
-        if(!alreadyAcknoweledged(id) && globalAcks.get(id).get.isEmpty){
-          println(s"from Persisted key $key OperationAck $id")
-          ackRequesters(id) ! OperationAck(id)
-          alreadyAcknoweledged+=id
+    case Insert(key, value, id) =>
+      kv += (key -> value)
+      persistAcks += (id -> sender)
+      if (replicators.nonEmpty) {
+        replicateAcks += (id -> (sender(), replicators))
+        replicators.foreach { replicator =>
+          replicator ! Replicate(key, Some(value), id)
         }
-        else println("Persisted cannot acknowledged operation remaining:"+globalAcks.get(id).get)
+      }
+      persistRepeaters += (id -> context.system.scheduler.schedule(0 millis, 100 millis, persistence, Persist(key, Some(value), id)))
+      failureGenerators += id -> context.system.scheduler.scheduleOnce(1 second) {
+        self ! GenerateFailure(id)
+      }
 
+    case Remove(key, id) =>
+      kv -= key
+      persistAcks += (id -> sender)
+      if (replicators.nonEmpty) {
+        replicateAcks += id -> (sender(), replicators)
+        replicators.foreach { replicator =>
+          replicator ! Replicate(key, None, id)
+        }
+      }
+      persistRepeaters += id -> context.system.scheduler.schedule(0 milli, 100 millis, persistence, Persist(key, None, id))
+      failureGenerators += id -> context.system.scheduler.scheduleOnce(1 second) { self ! GenerateFailure(id) }
 
+    case Persisted(key, id) =>
+      persistRepeaters(id).cancel()
+      persistRepeaters -= id
+      val origSender = persistAcks(id)
+      persistAcks -= id
+      if (!replicateAcks.contains(id)) {
+        failureGenerators(id).cancel()
+        failureGenerators -= id
+        origSender ! OperationAck(id)
+      }
 
-      case Replicas(reps) =>
-        //replicate kv store to new replica
-        reps
-          .filterNot(_ == self)//filter out primary
-          .filterNot(secondaries.get(_).isDefined)//filter out already known replicas
-          .foreach{
-            replica =>
-              val replicator = context.actorOf(Replicator.props(replica))//create Replicator
-              kv.foreach{ case (k,v) =>
-                replicator ! Replicate(k,Some(v),-1)//replicate kv store
-              }
-              secondaries +=((replica,replicator))//register new Replicator
-          }
+    case Replicated(key, id) =>
+      if (replicateAcks.contains(id)) {
+        val (origSender, currAckSet) = replicateAcks(id)
+        val newAckSet = currAckSet - sender
+        if (newAckSet.isEmpty)
+          replicateAcks -= id
+        else
+          replicateAcks = replicateAcks.updated(id, (origSender, newAckSet))
+        if (!replicateAcks.contains(id) && !persistAcks.contains(id)) {
+          failureGenerators(id).cancel()
+          failureGenerators -= id
+          origSender ! OperationAck(id)
+        }
+      }
 
-        println(s"Replicas secondaries is now $secondaries")
-        //remove replicator having replica not present anymore
-        val obsoleteReplicas = secondaries.filter{case (k,_) => !reps(k)}
-        println(s"obsolete replicas $obsoleteReplicas")
-        obsoleteReplicas.foreach(_._2 ! PoisonPill)//stop associated replicators
-        //update outstanding acks
-        globalAcks = globalAcks.mapValues(_ -- obsoleteReplicas.values)
-        //acknowledgment might be ready to be sent off now that some replicas are out
-        globalAcks
-          .filterNot{case(id,_) => alreadyAcknoweledged(id)}
-          .filter{case(_,v) => v.isEmpty}
-          .foreach {case (id, _) => ackRequesters(id) ! OperationAck(id)}
+    case GenerateFailure(id) =>
+      if (failureGenerators.contains(id)) {
+        if (persistRepeaters.contains(id)) {
+          persistRepeaters(id).cancel()
+          persistRepeaters -= id
+        }
+        failureGenerators -= id
+        val origSender =
+          if (persistAcks.contains(id)) persistAcks(id)
+          else replicateAcks(id)._1
+        persistAcks -= id
+        replicateAcks -= id
+        origSender ! OperationFailed(id)
+      }
 
-        //update secondaries refs removing obsolete replica
-        secondaries = secondaries -- obsoleteReplicas.keys
-        println(s"secondaries after removing obsolete replicas $secondaries")
-
-
-      case Replicated(key,id) =>
-        println(s"Replicated key:$key id:$id globalAck is $globalAcks, sender is $sender")
-        if(!alreadyAcknoweledged(id)){
-          val remainingReplicators = globalAcks.get(id)
-          if(remainingReplicators.isDefined){
-            globalAcks = globalAcks.updated(id,remainingReplicators.get - sender)
-            if(globalAcks.get(id).get.isEmpty){
-              println(s"from Replicated key $key OperationAck $id")
-              ackRequesters(id) ! OperationAck(id)
-              alreadyAcknoweledged+=id
+    case Replicas(replicas) =>
+      val secondaryReplicas = replicas.filterNot(_ == self)
+      val removed = secondaries.keySet -- secondaryReplicas
+      val added = secondaryReplicas -- secondaries.keySet
+      var addedSecondaries = Map.empty[ActorRef, ActorRef]
+      val addedReplicators = added.map { replica =>
+        val replicator = context.actorOf(Replicator.props(replica))
+        addedSecondaries += replica -> replicator
+        replicator
+      }
+      removed.foreach(replica => secondaries(replica) ! PoisonPill)
+      removed.foreach { replica =>
+        replicateAcks.foreach {
+          case (id, (origSender, rs)) =>
+            if (rs.contains(secondaries(replica))) {
+              self.tell(Replicated("", id), secondaries(replica))
             }
-          }
-          else println(s"Replicated recvd but cannot find replicator in globalAck for id $id!!")
         }
-        else println(s"operation id $id already acknowledged")
-
-      case FailGlobalAck(id) =>
-        println("in FailGlobalAck primary, globalAcks " + globalAcks)
-        if(!globalAcks(id).isEmpty){
-          ackRequesters(id)!OperationFailed(id)
-          alreadyAcknoweledged+=id
+      }
+      replicators = replicators -- removed.map(secondaries) ++ addedReplicators
+      secondaries = secondaries -- removed ++ addedSecondaries
+      addedReplicators.foreach { replicator =>
+        kv.zipWithIndex.foreach {
+          case ((k, v), idx) =>
+            replicator ! Replicate(k, Some(v), idx)
         }
-        //clean up already processed
-        globalAcks -= id
-    }
+      }
   }
 
+  /* Behavior for the replica role. */
   val replica: Receive = {
 
-    common.orElse{
+    case Get(key, id) => sender ! GetResult(key, kv.get(key), id)
 
-      case Snapshot(key, value, seq) =>
-        replicator=sender
-        println(s"Snapshot key:$key seq:$seq")
-        if (expectedSeq == seq) {
-          kv = value.fold(kv - key)(v => kv + ((key, v)))
-          expectedSeq = seq + 1
-          val persist = Persist(key, value, seq)
-          persistence ! persist
-          context.system.scheduler.scheduleOnce(100 milliseconds, self, ResendPersist(persist))
+    case Snapshot(key, valueOption, seq) =>
+      if (seq < snapshotSeq)
+        sender ! SnapshotAck(key, seq)
+
+      if (seq == snapshotSeq) {
+        valueOption match {
+          case None => kv -= key
+          case Some(value) => kv += key -> value
         }
-        else if (expectedSeq > seq)
-          replicator ! SnapshotAck(key, seq)
+        snapshotSeq += 1
+        persistAcks += seq -> sender
 
-      case Persisted(key, id) =>
-        println(s"sec.Persisted key $key id $id")
-        persisted = persisted + id
-        replicator ! SnapshotAck(key, id)
-    }
+        persistRepeaters += seq -> context.system.scheduler.schedule(0 milli, 100 millis, persistence, Persist(key, valueOption, seq))
+      }
 
+    case Persisted(key, id) =>
+      val sender = persistAcks(id)
+      persistAcks -= id
+      persistRepeaters(id).cancel()
+      persistRepeaters -= id
+      sender ! SnapshotAck(key, id)
   }
 
-
-  private def update(op:Operation, value:Option[String]):Unit={
-    val key=op.key
-    println(s"$op $key,$value")
-    globalAcks += ((op.id, Set(self)))
-    ackRequesters = ackRequesters.updated(op.id,sender)
-    secondaries.values.foreach {
-      replicator =>
-        replicator ! Replicate(key, value, op.id)
-        globalAcks = globalAcks.updated(op.id,globalAcks.get(op.id).get + replicator)
-    }
-    //persist
-    val persist = Persist(key, value, op.id)
-    persistence ! persist
-    //schedule an eventual resend of persistence message
-    context.system.scheduler.scheduleOnce(100 milliseconds, self, ResendPersist(persist))
-    //scheduler will make sure the global acknowledgment has been sent after 1 second
-    context.system.scheduler.scheduleOnce(1 second, self, FailGlobalAck(op.id))
-    println(s"$op globalAcks is now $globalAcks")
- }
 }
-
--- kvstore/src/main/scala/kvstore/Replicator.scala
+++ kvstore/src/main/scala/kvstore/Replicator.scala
@@ -1,74 +1,57 @@
 package kvstore
 
-import akka.actor.{Actor, ActorRef, Props}
+import akka.actor.{ Cancellable, Props, Actor, ActorRef }
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 object Replicator {
-
   case class Replicate(key: String, valueOption: Option[String], id: Long)
-
   case class Replicated(key: String, id: Long)
 
   case class Snapshot(key: String, valueOption: Option[String], seq: Long)
-
   case class SnapshotAck(key: String, seq: Long)
 
   def props(replica: ActorRef): Props = Props(new Replicator(replica))
 }
 
 class Replicator(val replica: ActorRef) extends Actor {
-
-  import kvstore.Replicator._
-
-  import scala.concurrent.duration._
-  import scala.concurrent.ExecutionContext.Implicits.global
-  import scala.language.postfixOps
+  import Replicator._
+  import context.dispatcher
 
   /*
    * The contents of this actor is just a suggestion, you can implement it in any way you like.
    */
 
-  /**
-   * In case Snapshot are not Acknowledged then we need to resend
-   */
-  private case object ResendSnapshot
-
   // map from sequence number to pair of sender and request
   var acks = Map.empty[Long, (ActorRef, Replicate)]
-
   // a sequence of not-yet-sent snapshots (you can disregard this if not implementing batching)
   var pending = Vector.empty[Snapshot]
 
-  var _seqCounter = 0L
+  var repeaters = Map.empty[Long, Cancellable]
 
+  var _seqCounter = 0L
   def nextSeq = {
     val ret = _seqCounter
     _seqCounter += 1
     ret
   }
 
+  /* Behavior for the Replicator. */
   def receive: Receive = {
-
-    case Replicate(key, value, id) =>
+    case replicate @ Replicate(key, valueOption, id) =>
       val seq = nextSeq
-      println(s"Replicate key $key id $id next seq is $seq")
-      acks = acks + ((seq, (replica, Replicate(key, value, id))))
-      replica ! Snapshot(key, value, seq)
+      acks += seq -> (sender(), replicate)
+      val snapshotRepeater = context.system.scheduler.schedule(0 milli, 250 millis, replica, Snapshot(key, valueOption, seq))
+      repeaters += seq -> snapshotRepeater
 
     case SnapshotAck(key, seq) =>
-      println(s"SnapshotAck key $key seq $seq")
-      acks = acks - seq //remove acknowledged
-      context.parent ! Replicated(key,seq)//acknowledge primary
-
-    case ResendSnapshot =>
-      println(s"ResendSnapshot")
-      acks.foreach {
-        case (seq, (rep, req)) => rep ! Snapshot(req.key, req.valueOption, seq)
+      if (acks.contains(seq)) {
+        val (sender, Replicate(key, _, id)) = acks(seq)
+        acks -= seq
+        repeaters(seq).cancel()
+        repeaters -= seq
+        sender ! Replicated(key, id)
       }
-
   }
-
-  //scheduler will resend Snapshot every 100 ms to achieve atLeastOnce Delivery to Replica
-  context.system.scheduler.schedule(100 milliseconds, 100 milliseconds, self, ResendSnapshot)
-
 
 }
